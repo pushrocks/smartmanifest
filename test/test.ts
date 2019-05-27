207183e5314ef76d483f063794d6e9303ebce3e0
Phil Kunz <gitlab@philkunz.com>
2019-05-27 13:35:07 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,19 @@
 import { expect, tap } from '@pushrocks/tapbundle';
-import * as smartmanifest from '../ts/index'
+import * as smartmanifest from '../ts/index';
+
+let testSmartManifest: smartmanifest.SmartManifest;
 
 tap.test('first test', async () => {
-  console.log(smartmanifest.standardExport)
-})
+  testSmartManifest = new smartmanifest.SmartManifest({
+    name: 'test app',
+    short_name: 'app'
+  });
+  expect(testSmartManifest).to.be.instanceOf(smartmanifest.SmartManifest);
+});
 
-tap.start()
+tap.test('should produce a web app manifest', async () => {
+  const testData = testSmartManifest.getData();
+  console.log(testData);
+});
+
+tap.start();
--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,56 @@
 import * as plugins from './smartmanifest.plugins';
 
-export let standardExport = 'Hi there! :) This is an exported string';
+export interface ISmartManifestConstructorOptions {
+  name: string;
+  short_name: string;
+  start_url?: '/';
+  display?: string;
+  orientation?: string;
+  background_color?: string;
+  theme_color?: string;
+  icons?: [
+    {
+      src: string;
+      type: string;
+      sizes: string;
+    }
+  ];
+}
+
+const defaultConstructorOptions: ISmartManifestConstructorOptions = {
+  name: 'UNNAMED APP',
+  short_name: 'UNNAMED',
+  start_url: '/',
+  display: 'standalone',
+  orientation: 'any',
+  background_color: '#fff',
+  theme_color: '#f78f21',
+  icons: [
+    {
+      src: '/assets/icon-large.png',
+      type: 'image/png',
+      sizes: '1024x1024',
+    }
+  ]
+}
+
+export class SmartManifest {
+  public options: ISmartManifestConstructorOptions;
+  constructor(optionsArg: ISmartManifestConstructorOptions) {
+    this.options = {
+      ...defaultConstructorOptions,
+      ...optionsArg
+    };
+  }
+
+  public jsonString(): string {
+    return JSON.stringify(this.options);
+  }
+
+  /**
+   * get the manifest data as javascriptObject
+   */
+  public getData() {
+    return JSON.parse(JSON.stringify(this.options));
+  }
+}
